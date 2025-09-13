24b82e09fe1e71a99ccf3285f8b83177f1ad3da4
Chetana Mistry <cpmistry20@gmail.com>
2025-09-13 23:44:16 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,22 +12,36 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 //parse JSON bodies 
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/api/notes", notesRoutes); 
+app.use("/api/notes", notesRoutes);
 
-connectDB().then( () => {
-    app.listen(5001, () => { 
-        console.log("Server started on PORT:", PORT);   
+// Serve static files from the React frontend app
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+connectDB().then(() => {
+    app.listen(5001, () => {
+        console.log("Server started on PORT:", PORT);
     });
 });
 
